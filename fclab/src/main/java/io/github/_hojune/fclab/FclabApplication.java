5c45465247ce34c6a18f85d21142429f66193860
Hojune Lee <hojune0124@gmail.com>
2026-03-11 17:47:58 +0900

    chore: Spring Boot 초기 프로젝트 세팅

---
+++ fclab/src/main/java/io/github/_hojune/fclab/FclabApplication.java
@@ -0,0 +1,13 @@
+package io.github._hojune.fclab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FclabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FclabApplication.class, args);
+	}
+
+}
---
+++ fclab/src/test/java/io/github/_hojune/fclab/FclabApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github._hojune.fclab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FclabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
